d0e3bbc6dc5b6f2b4ca4ac472e73973b88773666
GabrielDSilvaa <gabrieldaaaaaaasilva1015@gmail.com>
2025-06-09 13:54:39 -0300

    Update 3-B.cpp

--- 3/3-B.cpp
+++ 3/3-B.cpp
@@ -1,8 +1,21 @@
-O quicksort é mais eficiente em listas
-grandes porque usa a estratégia de dividir e conquistar, 
-separando os elementos com base em um pivô e ordenando as 
-partes menores de forma recursiva. Ele tem uma complexidade 
-média de O(n log n) e pode ser implementado sem usar muita
-memória extra, o que o torna rápido 
-e leve para grandes volumes de dados. Além disso, com uma 
-boa escolha do pivô, evita seu pior caso (O(n²)).
+int particionar(vector<int>& vetor, int inicio, int fim) {
+    int pivo = vetor[fim];
+    int i = inicio - 1;
+
+    for (int j = inicio; j < fim; j++) {
+        if (vetor[j] <= pivo) {
+            i++;
+            swap(vetor[i], vetor[j]);
+        }
+    }
+    swap(vetor[i + 1], vetor[fim]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& vetor, int inicio, int fim) {
+    if (inicio < fim) {
+        int p = particionar(vetor, inicio, fim);
+        quickSort(vetor, inicio, p - 1);
+        quickSort(vetor, p + 1, fim);
+    }
+}
