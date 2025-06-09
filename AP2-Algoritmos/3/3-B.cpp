5faeac9f1477f5060cd8ddf064ec1cba40e8dbc7
GabrielDSilvaa <gabrieldaaaaaaasilva1015@gmail.com>
2025-06-08 21:40:20 -0300

    Add files via upload

---
+++ AP2-Algoritmos/3/3-B.cpp
@@ -0,0 +1,8 @@
+O quicksort é mais eficiente em listas
+grandes porque usa a estratégia de dividir e conquistar, 
+separando os elementos com base em um pivô e ordenando as 
+partes menores de forma recursiva. Ele tem uma complexidade 
+média de O(n log n) e pode ser implementado sem usar muita
+memória extra, o que o torna rápido 
+e leve para grandes volumes de dados. Além disso, com uma 
+boa escolha do pivô, evita seu pior caso (O(n²)).
